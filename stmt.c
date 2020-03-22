677211153a2e70ac08cc243a1fd98bae1356b865
Brayden Marshall <bmarsh579@gmail.com>
2020-03-21 22:15:58 -0700

    Started adding proper indentation to stmt_print.

--- stmt.c
+++ stmt.c
@@ -53,53 +53,91 @@ struct stmt* stmt_create_return(struct expr* expr) {
     return s;
 }
 
-void stmt_print(struct stmt* s) {
+void indent_print(char* string, int level);
+void _stmt_print(struct stmt* s, int indent);
+void body_print(struct stmt *body, int indent_level, int indent_first);
+void stmt_print(struct stmt* s);
+
+void indent_print(char* string, int level) {
+    for (int i = 0; i < level; i++) {
+        printf("    ");
+    }
+
+    printf("%s", string);
+}
+
+// indent_first == boolean
+void body_print(struct stmt *s, int indent_level, int indent_first) {
+    if (s->kind == STMT_BLOCK) {
+        if (indent_first) {
+            indent_print("{\n", indent_level);
+        } else {
+            printf("{\n");
+        }
+        _stmt_print(s->body, indent_level + 1);
+        indent_print("}\n", indent_level);
+    } else {
+        printf("\n");
+        _stmt_print(s->body, indent_level + 1);
+    }
+}
+
+void _stmt_print(struct stmt* s, int indent) {
     if (!s) return;
 
     switch (s->kind) {
         case STMT_DECL:
+            indent_print("", indent);
             decl_print(s->decl);
             break;
         case STMT_EXPR:
+            indent_print("", indent);
             expr_print(s->expr);
             break;
         case STMT_IF_ELSE:
-            printf("if (");
+            indent_print("if (", indent);
             expr_print(s->expr);
             printf(") ");
-            stmt_print(s->body);
+            body_print(s->body, indent, 0);
+            //_stmt_print(s->body, indent+1);
 
             if (s->else_body) {
-                printf(" else (");
+                indent_print("else (", indent);
                 expr_print(s->expr);
                 printf(") ");
-                stmt_print(s->body);
+                body_print(s->body, indent, 0);
+                //_stmt_print(s->body, indent);
             }
             break;
         case STMT_FOR:
-            printf("for (");
+            indent_print("for (", indent);
             expr_print(s->init_expr);
             printf("; ");
             expr_print(s->expr);
             printf("; ");
             expr_print(s->next_expr);
-            printf(") ");
-            stmt_print(s->body);
+            printf(")\n");
+            _stmt_print(s->body, indent+1);
             break;
         case STMT_PRINT:
             printf("FIXME: printing print statements.\n");
             break;
         case STMT_RETURN:
-            printf("return ");
+            indent_print("return ", indent);
             expr_print(s->expr);
             printf(";\n");
             break;
         case STMT_BLOCK:
-            printf("{\n");
-            stmt_print(s->body);
-            printf("}\n");
+            //indent_print("{\n", indent);
+            //_stmt_print(s->body, indent+1);
+            //indent_print("}\n", indent);
+            body_print(s, indent, 1);
             break;
     }
 
-    stmt_print(s->next);
+    _stmt_print(s->next, indent);
+}
+
+void stmt_print(struct stmt* s) {
+    _stmt_print(s, 0);
 }
